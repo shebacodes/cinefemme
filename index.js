6e36a671cb1e8f89ebdc7720b3f7007484a3edb8
shebacodes <33535425+shebacodes@users.noreply.github.com>
2020-02-16 14:54:39 -0800

    added files to repo

---
+++ index.js
@@ -0,0 +1,13 @@
+/*var formStorage = function() {
+  alert("you clicked her");
+};
+formStorage();
+
+//function pushes data into an array or object*/
+
+var formStorage = [];
+function myFunction() {
+  var x = document.getElementById("container");
+  Number.push(document.getElementById("input".value));
+  x.innerHTML = number.join("br/>");
+}
